eb12eb702fe5eb96ffde097c745f7a799a5e1068
Saurabh <saurabhgaggar27@gmail.com>
2018-09-27 19:38:47 +0530

    redister user servlet

--- src/businessLogic/RegisterUser.java
+++ src/businessLogic/RegisterUser.java
@@ -1,6 +1,7 @@
 package businessLogic;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -25,8 +26,38 @@ public class RegisterUser extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		System.out.println("got post");
+		
+		String fullname=request.getParameter("fullname");
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		String repassword=request.getParameter("repassword");
+		String contact=request.getParameter("contact");
+		String balance=request.getParameter("balance");
+		String agreeterms=request.getParameter("agreeterms");
+		System.out.println(agreeterms);
+		String messageOnJsp="Invalid Input";
+		
+		if(agreeterms == null)
+		{	//not agreed to terms
+			messageOnJsp="Please Agree to Terms";
+		}else if(!password.equals(repassword)){
+			messageOnJsp="Passwords dont match";
+		}else {
+			//registration sucessful
+		}
+		ValidationImpl validation=new ValidationImpl();
+		int res=validation.ValidateUser(fullname);
+		if(res==1);//go ahead
+		else if(res==0)//already exists
+		
+		
+		
+		request.setAttribute("errorString", messageOnJsp);
+		request.getRequestDispatcher("register.jsp").forward(request, response);
+		//PrintWriter out = response.getWriter();
+	    // out.println("<h1>" + fullname+email+password+contact+balance+agreeterms+ "</h1>");
 	}
 
 }
