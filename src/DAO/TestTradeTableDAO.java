37c4c57735673de7236afcb0a90572938276a182
Saurabh <saurabhgaggar27@gmail.com>
2018-09-27 09:54:34 +0530

    trade table test

---
+++ src/DAO/TestTradeTableDAO.java
@@ -0,0 +1,47 @@
+package DAO;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import pojo.Trade;
+
+public class TestTradeTableDAO {
+	/*
+	 * public int DropTradeTable();
+	public List<Trade> GetAllTrades();
+	public int AddTrade(Trade trade);
+	public int GetCount();
+	public int GetCountUser(double user_id);
+	public List<Trade> GetTradesByUserId(Double UserId, int quantity);
+	 */
+	
+	@Test
+	public void testGetAllTrades() {
+		
+	}
+	
+	@Test
+	public void testAddTrade() {
+		
+	}
+	
+	@Test
+	public void testGetCount() {
+		
+	}
+	
+	@Test
+	public void testGetCountUser() {
+		
+	}
+	
+	@Test
+	public void testGetTradesByUserId() {
+		
+	}
+	
+	
+	
+
+}
