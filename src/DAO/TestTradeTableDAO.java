a73a41ff5cf8d10851773f22cffb5c9a45ff87c3
Saurabh <saurabhgaggar27@gmail.com>
2018-09-27 11:11:56 +0530

    trade table dao done

--- src/DAO/TestTradeTableDAO.java
+++ src/DAO/TestTradeTableDAO.java
@@ -1,5 +1,6 @@
 package DAO;
 
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -16,29 +17,39 @@ public class TestTradeTableDAO {
 	public List<Trade> GetTradesByUserId(Double UserId, int quantity);
 	 */
 	
+	TradeTableImpl impl=new TradeTableImpl();
+	
 	@Test
 	public void testGetAllTrades() {
-		
+		List<Trade> list=impl.GetAllTrades();
+		list.forEach(System.out::println);
 	}
 	
 	@Test
 	public void testAddTrade() {
-		
+		Trade trade=new Trade(81, 91, 99, 88, 100, 100, new Date());
+		int ret=impl.AddTrade(trade);
+		//System.out.println(ret);
+		assert(ret==1);
 	}
 	
 	@Test
 	public void testGetCount() {
-		
+		int ret=impl.GetCount();
+		System.out.println(ret);
 	}
 	
 	@Test
 	public void testGetCountUser() {
-		
+		int ret=impl.GetCountUser(88d);
+		System.out.println(ret);
 	}
 	
 	@Test
 	public void testGetTradesByUserId() {
-		
+		List<Trade> list=impl.GetTradesByUserId(88d, 10);
+		System.out.println(list.size());
+		list.forEach(System.out::println);
 	}
 	
 	
--- src/DAO/TradeTableImpl.java
+++ src/DAO/TradeTableImpl.java
@@ -34,16 +34,11 @@ public class TradeTableImpl implements TradeTable {
 
 	@Override
 	public List<Trade> GetAllTrades() {
-		// TODO Auto-generated method stub
 		
 		List<Trade> tradesall = new ArrayList<>();
 		
 		String GETALLTRADES = "SELECT * FROM TRADE_DETAILS ORDER BY traded_time DESC";
-		String uidbuy = "SELECT order_details.user_id_order FROM order_details INNER JOIN trade_details"
-				+ " ON order_details.order_id = trade_details.order_id_buy";
 		
-		String uidsell = "SELECT order_details.user_id_order FROM order_details INNER JOIN trade_details"
-				+ " ON order_details.order_id = trade_details.order_id_sell";
 		
 		try(Connection con = MyConnection.openConnection();) {
 			PreparedStatement ps = con.prepareStatement(GETALLTRADES);
@@ -54,17 +49,19 @@ public class TradeTableImpl implements TradeTable {
 				Date trade_time = set.getDate("traded_time");
 				double quantity = set.getDouble("traded_quantity");
 				double tradeprice = set.getDouble("traded_price");
+				double useridbuy = set.getDouble("user_id_buy");
+				double useridsell = set.getDouble("user_id_sell"); 
 				double orderidbuy = set.getDouble("order_id_buy");
 				double orderidsell = set.getDouble("order_id_sell");
 				
-				Trade alltrades = new Trade(orderidbuy, orderidsell, Double.parseDouble(uidbuy), Double.parseDouble(uidsell), tradeprice, quantity, trade_time);
-				tradesall.add(alltrades);
+				Trade trade = new Trade(orderidbuy, orderidsell, useridbuy, useridsell, tradeprice, quantity, trade_time);
+				trade.setTradeId(tradeid);
+				tradesall.add(trade);
 				
 			}
 
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return tradesall;
@@ -74,25 +71,25 @@ public class TradeTableImpl implements TradeTable {
 
 	@Override
 	public int AddTrade(Trade trade) {
-		// TODO Auto-generated method stub
+
 		int rowsAdded = 0;
-		String ADDTRADE = "insert into trade_details values(?,?,?,?,?,?)";
+		String ADDTRADE = "insert into trade_details values(?,?,?,?,?,?,?)";
 		
 		Connection con = MyConnection.openConnection();
 		try {
 			PreparedStatement ps = con.prepareStatement(ADDTRADE);
 			
-			ps.setDouble(1, trade.getTradeId());
-			ps.setDouble(2, trade.getOrderId_buy());
-			ps.setDouble(3, trade.getOrderId_sell());
-			ps.setDouble(4, trade.getTradedPrice());
-			ps.setDouble(5, trade.getTradedQuantity());
-			ps.setObject(6, new java.sql.Timestamp(trade.getTradedTime().getTime()));
-//			ps.setDate(6, (java.sql.Date) trade.getTradedTime());
+			ps.setDouble(1, trade.getOrderId_buy());
+			ps.setDouble(2, trade.getOrderId_sell());
+			ps.setDouble(3, trade.getUserId_buy());
+			ps.setDouble(4, trade.getUserId_sell());
+			ps.setDouble(5, trade.getTradedPrice());
+			ps.setDouble(6, trade.getTradedQuantity());
+			ps.setObject(7, new java.sql.Timestamp(trade.getTradedTime().getTime()));
+			//ps.setDate(6, (java.sql.Date) trade.getTradedTime());
 			
 			rowsAdded = ps.executeUpdate();		
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return rowsAdded;
@@ -100,18 +97,21 @@ public class TradeTableImpl implements TradeTable {
 
 	@Override
 	public int GetCount() {
-		// TODO Auto-generated method stub
+		//gets todays trades count
 		int count = 0;
 		
-		String getcount = "SELECT COUNT(trade_id) from trade_details WHERE DATEPART(DD, traded_time) = DATEPART(DD, GETDATE())"
+		String getcount = "SELECT COUNT(trade_id) as cnt from trade_details WHERE DATEPART(DD, traded_time) = DATEPART(DD, GETDATE())"
 						+ "AND DATEPART(MM, traded_time) = DATEPART(MM, GETDATE()) "
-						+ "AND DATEPART(YYYY, traded_time) = DATEPART(YYYY, GETDATE())";		
+						+ "AND DATEPART(YYYY, traded_time) = DATEPART(YYYY, GETDATE())";	
+		
 		try(Connection con = MyConnection.openConnection();) {
 			PreparedStatement ps = con.prepareStatement(getcount);
 			
-			count = ps.executeUpdate();
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				count=rs.getInt("cnt");
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return count;
@@ -119,19 +119,21 @@ public class TradeTableImpl implements TradeTable {
 
 	@Override
 	public int GetCountUser(double user_id) {
-		// TODO Auto-generated method stub
-
+		//gets number of trades by user today
 		int count = 0;
 		
-		String getcount = "SELECT COUNT(trade_id) from trade_details WHERE user_id = ? AND DATEPART(DD, traded_time) = DATEPART(DD, GETDATE())"
+		String getcount = "SELECT COUNT(trade_id) as cnt from trade_details WHERE user_id_buy = ? OR user_id_sell = ? AND DATEPART(DD, traded_time) = DATEPART(DD, GETDATE())"
 						+ "AND DATEPART(MM, traded_time) = DATEPART(MM, GETDATE()) "
 						+ "AND DATEPART(YYYY, traded_time) = DATEPART(YYYY, GETDATE())";		
 		try(Connection con = MyConnection.openConnection();) {
 			PreparedStatement ps = con.prepareStatement(getcount);
 			ps.setDouble(1, user_id);
-			count = ps.executeUpdate();
+			ps.setDouble(2, user_id);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				count=rs.getInt("cnt");
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return count;
@@ -139,9 +141,12 @@ public class TradeTableImpl implements TradeTable {
 
 	@Override
 	public List<Trade> GetTradesByUserId(Double UserId, int quantity) {
-		// TODO Auto-generated method stub
-		List<Trade> tradesid = new ArrayList<>();
+		//gets quantity number of trades by userid
 		
+		List<Trade> trades = new ArrayList<>();
+		String quer="select * from trade_details where user_id_buy = ? OR user_id_sell = ? order by traded_time desc";
+		
+		/*
 		String GETORDERID = "SELECT order_id FROM order_details WHERE user_id_order = ?";
 		String GETTRADEBYOID = "SELECT * FROM trade_details WHERE order_id_buy = ? OR order_id_sell = ?";
 		
@@ -150,35 +155,32 @@ public class TradeTableImpl implements TradeTable {
 		
 		String uidsell = "SELECT order_details.user_id_order FROM order_details INNER JOIN trade_details"
 				+ " ON order_details.order_id = trade_details.order_id_sell";
-
+		 */
+		
 		try(Connection con = MyConnection.openConnection();) {
-			PreparedStatement ps = con.prepareStatement(GETORDERID);
+			PreparedStatement ps = con.prepareStatement(quer);
 			ps.setDouble(1, UserId);
+			ps.setDouble(2, UserId);
 			ResultSet set = ps.executeQuery();
 			while(set.next())
 			{
-				PreparedStatement ps1 = con.prepareStatement(GETTRADEBYOID);
-				ps1.setDouble(1, set.getDouble("order_id"));
-				ps1.setDouble(2, set.getDouble("order_id"));
-				ResultSet set1 = ps1.executeQuery();
-				while(set1.next())
-				{
-					double orderidbuy = set.getDouble("order_id_buy");
-					double orderidsell = set.getDouble("order_id_sell");
-					double tradedprice = set.getDouble("traded_price");
-					double quant = set.getDouble("traded_quantity");
-					Date tradedtime = set.getDate("traded_time");
-					
-					Trade trade1 = new Trade(orderidbuy, orderidsell, Double.parseDouble(uidbuy), Double.parseDouble(uidsell), tradedprice, quant, tradedtime);
-					tradesid.add(trade1);
-
-				}
+				double tradeid = set.getDouble("trade_id");
+				Date trade_time = set.getDate("traded_time");
+				double quantity2 = set.getDouble("traded_quantity");
+				double tradeprice = set.getDouble("traded_price");
+				double useridbuy = set.getDouble("user_id_buy");
+				double useridsell = set.getDouble("user_id_sell"); 
+				double orderidbuy = set.getDouble("order_id_buy");
+				double orderidsell = set.getDouble("order_id_sell");
+				
+				Trade trade = new Trade(orderidbuy, orderidsell, useridbuy, useridsell, tradeprice, quantity2, trade_time);
+				trade.setTradeId(tradeid);
+				trades.add(trade);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tradesid;
+		return trades;
 
 	}
 }
